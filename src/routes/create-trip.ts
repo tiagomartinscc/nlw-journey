5152186e26ecc59dc11e72fa495dc46e07850304
Tiago Martins <tiagomartinscc@hotmail.com>
2024-07-10 00:08:13 -0300

    route confirm participant

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod"
 import { prisma } from "../lib/prisma";
-import dayjs from "dayjs"
 import { getMailClient } from "../lib/mail";
 import nodemailer from "nodemailer"
+import { dayjs } from "../lib/dayjs";
 
 export async function createTrip(app:FastifyInstance) {
    await app.withTypeProvider<ZodTypeProvider>().post('/trips', {
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { createTrip } from "./routes/create-trip"
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import cors from "@fastify/cors"
+import { confirmParticipant } from "./routes/confirm-participant";
 
 const app = fastify()
 app.register(cors, {
@@ -14,6 +15,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
 
 app.listen({port: 3333}).then(() => {
   console.log("server running...")
